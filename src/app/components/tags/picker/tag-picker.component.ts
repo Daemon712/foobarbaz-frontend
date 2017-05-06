5783ce14ed3da319bea6567351cceab61a01b3ed
Dmitriy Baboshkin <Daemon712@yandex.ru>
2017-05-07 00:46:28 +0400

    tag picker fixes

--- src/app/components/tags/picker/tag-picker.component.ts
+++ src/app/components/tags/picker/tag-picker.component.ts
@@ -31,7 +31,7 @@ export class TagPickerComponent implements OnInit {
             .findTags(newTag)
             .then(tags => {
               let result = tags.map(tag => tag.name);
-              if (this.newTag && this.newTag.length >= 3) result.push(this.newTag);
+              if (this.newTag && this.newTag.length >= 3) result.push(this.newTag.toLowerCase());
               return this.tags ? result.filter(tagName => this.tags.indexOf(tagName) < 0) : result
             })
       );
